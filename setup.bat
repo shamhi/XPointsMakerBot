00042fd6477353ccc1e5d88bd6b68e4dc207d60b
AlexAr GC <alex_gc@mail.ru>
2024-08-19 23:09:33 +0500

    first release v.1.3

---
+++ setup.bat
@@ -0,0 +1,4 @@
+@REM Пример запуска установщика с версией 3.11
+@REM py -3.11 -m pip install -r requirements.txt
+pip install -r requirements.txt
+playwright install
---
+++ start-farm.bat
@@ -0,0 +1,3 @@
+@REM Пример запуска с версией 3.11
+@REM py -3.11 main.py
+py main.py
